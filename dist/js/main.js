2d91c52eb0f424cce9a6e4d04e8793811a5def34
Madays <maday.choque@gmail.com>
2017-03-28 18:31:21 -0500

    subiendo carpeta dist

---
+++ dist/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    mostrarInfo();
+    mostrarbtnBuscar();
+    dibujarOpcionesCiudad();
+    mostrarbtnBuscar();
+    precioRuta();
+    validadCantidadPasajeros();
+        
+});
+function mostrarInfo(){
+    $('#box-icon').click(function(){
+        $('#info').slideToggle("slow"); 
+    });
+}
+
+function mostrarbtnBuscar(){
+    
+}
+
+function precioRuta(){
+    var ciudades = get_regiones();
+    var consumoLkm = {
+        auto: 12,
+        moto: 21,
+        minivan: 7,
+        camion: 6
+    }
+    $('#btn-buscar').click(function(){
+        $('#calculator').slideToggle("slow"); 
+        var distancia = parseInt($('#origen').val()) + parseInt($('#destino').val());
+        var precioMotocicleta = distancia/21*673;
+        var precioAutomovil = distancia/12*673;
+        var precioMinivan = distancia/7*673;
+        var precioCamion = distancia/6*673;
+        $('#precioMotocicleta').text(precioMotocicleta);
+        $('#precioAutomovil').text(precioAutomovil);
+        $('#precioMinivan').text(precioMinivan);
+        $('#precioCamion').text(precioCamion);
+    });        
+}
+function validadCantidadPasajeros(){        
+    $('#cant-pasajeros').keyup(function(){
+        alert('5678');
+        var regex = /([0-9]+)/;//expresion de numeros del 1 al 9
+        var numPasajero=parseInt($('#cant-pasajeros').val());
+        if(numPasajero!==regex){
+            alert('df');
+        }   
+    });
+    
+    
+}
+
