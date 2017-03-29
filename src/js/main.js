02661adad6d1979f9ef9e4d7f3410330ca805c93
Madays <maday.choque@gmail.com>
2017-03-29 11:35:06 -0500

    fondo con api googlemap

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,55 @@
 $(document).ready(function(){
+    initMap();
     mostrarInfo();
     mostrarbtnBuscar();
     dibujarOpcionesCiudad();
     mostrarbtnBuscar();
     precioRuta();
-    validadCantidadPasajeros();
+    validadCantidadPasajeros(); 
+    elegirVehiculo();
     
     $('#btn-buscar').hide();
     $('#calculator').hide();
+    $('#compartir-carro').hide();
 });
 
+function initMap() {
+    getLocation();
+}
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+    var myLatLng = {lat:position.coords.latitude,lon:position.coords.longitude};
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: myLatLng.lat, lng: myLatLng.lon},
+    zoom: 8
+  });
+    
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: {lat: myLatLng.lat, lng: myLatLng.lon},
+        title: 'Hello World!',
+        /*icon:"car.png"*/
+    });
+    
+    var myLatLng1 = [
+      {lat: -12.1511485, lng: -70.531948},{lat: -15.4521485, lng: -73.532948},{lat: -15.4521485, lng: -71.532948}];
+    //{lat: -15.4521485, lng: -71.532948},{lat: -16.4531485, lng: -74.533948},{lat: -15.4541485, lng: -73.534948},{lat: -16.4571485, lng: -71.532948}
+    for(var i=0;i<=myLatLng1.length;i++){
+        var marker1 = new google.maps.Marker({
+          map: map,
+          position: myLatLng1[i],            
+          title: 'Hello World!',
+          /*icon:"../img/car.png"*/
+        });    
+    }    
+}
 function mostrarInfo(){
     $('#box-icon').click(function(){
         $('#info').slideToggle("slow"); 
@@ -53,6 +93,13 @@ function validadCantidadPasajeros(){
             console.log(numPasajero);
             alert('Ingrese un numero');
         }*/   
+    
+}
+var vehiculo=false;
+function elegirVehiculo(){
+    $('input[type=radio]').change(function() {             return vehiculo=true;
+        console.log(vehiculo);                                      
+    });
     
 }
 
